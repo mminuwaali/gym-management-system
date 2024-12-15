babd5524cfb43e02c99b6535ee9a6104caa9057c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-15 22:09:02 +0100

    modifed the auth service and added a method to get user data

--- src/apps/services/auth.service.ts
+++ src/apps/services/auth.service.ts
@@ -1,32 +1,46 @@
+import { Injectable } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import * as authUtil from 'src/utilities/auth.util';
 import { AuthRepository } from '../repositories/auth.repository';
-import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  constructor(private authRepository:AuthRepository){} 
+  constructor(private authRepository: AuthRepository) {}
 
-  async login(data: Pick<User, "email"|"password">){ 
+  async getCurrentUser(email: string) {
+    const user = await this.authRepository.findByEmail(email);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    return user;
+  }
+
+  async login(data: Pick<User, 'email' | 'password'>) {
     const user = await this.authRepository.findByEmail(data.email);
-    if(!user) throw new UnauthorizedException('Invalid credentials');
+    if (!user) throw new UnauthorizedException('Invalid credentials');
 
-    const isMatch = await authUtil.comparePasswords(data.password, user.password);
+    const isMatch = await authUtil.comparePasswords(
+      data.password,
+      user.password,
+    );
     if (!isMatch) throw new UnauthorizedException('Invalid credentials');
-  
+
     const payload = { email: user.email, role: user.role };
-    return authUtil.generateJwtToken(payload);
-  };
-  
-  async register(data: Partial<User>){ 
+    return this.generateToken(payload);
+  }
+
+  async register(data: Partial<User>) {
     const user = await this.authRepository.findByEmail(data.email);
-    if(user) throw new ConflictException('User already exists');
+    if (user) throw new ConflictException('User already exists');
 
-    const isMatch = await authUtil.comparePasswords(data.password, user.password);
+    const isMatch = await authUtil.comparePasswords(
+      data.password,
+      user.password,
+    );
     if (!isMatch) throw new UnauthorizedException('Invalid credentials');
-  };
+  }
 
-  private generateToken(payload:Record<"email"|"role", string>) {
+  private generateToken(payload: Record<'email' | 'role', string>) {
     return authUtil.generateJwtToken(payload);
   }
 }

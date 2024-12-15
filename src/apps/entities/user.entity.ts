fe11f449b7371a44d7f5e7e001ef88d132cce73d
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-15 20:42:54 +0100

    setup the user entity in use for the auth repository

---
+++ src/apps/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, OneToMany } from 'typeorm';
+import { BaseEntity } from 'src/shared/database/entities/base.entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @Column()
+  password: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'enum', enum: ['Super Admin', 'Gym Owner', 'Instructor', 'Gym User'] })
+  role: RoleType;
+}

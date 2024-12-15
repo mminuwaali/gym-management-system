dc6d16c81faeefc7b4f24fc0402190b8a7e69f0f
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-15 22:09:46 +0100

    formatted and registered user endpoint to fetch authenticated user's data

--- src/apps/controllers/auth.controller.ts
+++ src/apps/controllers/auth.controller.ts
@@ -1,18 +1,25 @@
 import { User } from '../entities/user.entity';
-import { Post, Controller } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { Get, Post, Controller, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/shared/webtoken/webtoken.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Post("login")
-    async login (data:Pick<User, "email"|"password">){
-        return await this.authService.login(data);
-    }
+  @Get('user')
+  @UseGuards(JwtAuthGuard)
+  async user() {
+    return await this.authService.getCurrentUser('');
+  }
 
-    @Post("register")
-    async register (data:Partial<User>){
-        return await this.authService.register(data);
-    }
+  @Post('login')
+  async login(data: Pick<User, 'email' | 'password'>) {
+    return await this.authService.login(data);
+  }
+
+  @Post('register')
+  async register(data: Partial<User>) {
+    return await this.authService.register(data);
+  }
 }

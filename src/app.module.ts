e683dcfb0c6d7cf09849c17c172aa57aeae9e9d7
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-15 23:07:10 +0100

    registered plan and subscription's repositories, services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,18 @@ import { WebtokenModule } from './shared/webtoken/webtoken.module';
 
 // services
 import { AuthService } from './apps/services/auth.service';
+import { PlanService } from './apps/services/plan.service';
+import { SubsriptionService } from './apps/services/subs.service';
 
 // repositoryies
 import { AuthRepository } from './apps/repositories/auth.repository';
+import { PlanRepository } from './apps/repositories/plan.repository';
+import { SubsriptionRepository } from './apps/repositories/subs.repository';
 
 // controllers
 import { AuthController } from './apps/controllers/auth.controller';
+import { PlanController } from './apps/controllers/plan.controller';
+import { SubsriptionController } from './apps/controllers/subs.controller';
 
 @Module({
   imports: [
@@ -21,7 +27,15 @@ import { AuthController } from './apps/controllers/auth.controller';
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   exports: [AuthRepository],
-  controllers: [AuthController],
-  providers: [AuthService, AuthRepository],
+  controllers: [AuthController, PlanController, SubsriptionController],
+  providers: [
+    AuthService,
+    PlanService,
+    SubsriptionService,
+
+    AuthRepository,
+    PlanRepository,
+    SubsriptionRepository,
+  ],
 })
 export class AppModule {}

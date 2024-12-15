903fc27ed216b4e326ea29d3902a07436777d3f6
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-12-15 20:46:11 +0100

    registered the auth controller, service and repository into the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+// modules
 import { DatabaseModule } from './shared/database/database.module';
 import { WebtokenModule } from './shared/webtoken/webtoken.module';
 
-@Module()
+// services
+import { AuthService } from './apps/services/auth.service';
+
+// repositoryies
+import { AuthRepository } from './apps/repositories/auth.repository';
+import { AuthController } from './apps/controllers/auth.controller';
+
+// controllers
+
+
+
+@Module({
+  imports:[
+    DatabaseModule,
+    WebtokenModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  providers: [
+    AuthService,
+    AuthRepository,
+  ],
+  exports:[
+    AuthRepository,
+  ],
+  controllers: [AuthController],
+})
 export class AppModule {}
